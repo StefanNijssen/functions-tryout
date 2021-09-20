30535791a9561eeb9158269952466406eb4cd59b
StefanNijssen <58977715+StefanNijssen@users.noreply.github.com>
2021-09-20 09:34:11 +0200

    tellen

---
+++ Tellen.py
@@ -0,0 +1,24 @@
+def addition(number1, number2):
+    aantal = number1 + number2
+    return aantal
+def substraction(number1, number2):
+    aantal = number1 - number2
+    return aantal
+def multiplication(number1, number2):
+    aantal = number1 * number2
+    return aantal
+def division(number1, number2):
+    aantal = number1 / number2
+    return aantal
+def increment(number):
+    aantal = number + 1
+    return aantal
+def decrement(number):
+    aantal = number - 1
+    return aantal
+print(addition(10,12))
+print(substraction(58,34))
+print(multiplication(6,7))
+print(division(144,12))
+print(increment(12))
+print(decrement(34))
